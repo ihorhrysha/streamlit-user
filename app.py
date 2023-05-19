7fecc3695f764ca6f8905062cbf431bfe27a1177
“Ihor <ihor.hrysha@gmail.com>
2023-05-19 16:34:55 +0300

    add headers info

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+from streamlit.web.server.websocket_headers import _get_websocket_headers
+
+headers = _get_websocket_headers()
 
 st.write("Hello")
 
 st.write(st.experimental_user)
+
+st.write(headers)
